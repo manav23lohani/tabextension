1c39e1ccfb73639934c5877866f10e377f6cce07
manav23lohani <manav23lohani@gmail.com>
2023-07-07 11:21:44 +0530

    load saved group done

--- popup.js
+++ popup.js
@@ -1,33 +1,29 @@
-const saveBtn = document.getElementById('saveButton');
-const loadBtn = document.getElementById('openTabs');
-saveBtn.addEventListener('click', () => { 
-    chrome.tabs.query({}, function(tabs) {
-        var tabGroup = [];
-      
-        tabs.forEach(function(tab) {
-            // console.log(tab.url);
-          var tabInfo = {
-            url: tab.url,
-            title: tab.title
-          };
-          tabGroup.push(tabInfo);
-        });
-      
-        // Store tabGroup in local storage
+const saveBtn = document.getElementById("saveButton");
+const loadBtn = document.getElementById("loadButton");
+const inputName = document.getElementById("inputName");
 
+saveBtn.addEventListener("click", () => {
+  const name = inputName.value;
+  if (name) {
+    chrome.tabs.query({}, function (tabs) {
+      var tabGroup = [];
+
+      tabs.forEach(function (tab) {
+        // console.log(tab.url);
+        tabGroup.push(tab.url);
+      });
+      localStorage.setItem(name, JSON.stringify(tabGroup));
     });
+  }
 });
-  
 
-  
-
-
-
-
-
-// chrome.storage.local.get(['tabGroup'], function(result) {
-//     var storedTabGroup = result.tabGroup;
-//     if (storedTabGroup) {
-//       console.log(storedTabGroup);
-//     }
-//   });
+loadBtn.addEventListener("click", () => {
+  const name = inputName.value;
+  if (name) {
+    const tabs = JSON.parse(localStorage.getItem(name));
+    // console.log(tabs);
+    tabs.forEach((url)=>{
+      window.open(url, "_blank");
+    })
+  }
+});
